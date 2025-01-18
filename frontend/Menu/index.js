58f3e4afb4ccc3aba8b557a8aac10c99f889b632
anit baranwal <anitbanawal2018@gmail.com>
2025-01-18 15:13:09 +0530

    frontend added

---
+++ frontend/Menu/index.js
@@ -0,0 +1,54 @@
+
+    // Add individual event listeners for each box
+    document.getElementById('box1').addEventListener('click', function() {
+      window.open('page1.html', '_blank');
+    });
+
+    document.getElementById('box2').addEventListener('click', function() {
+      window.open('page2.html', '_blank');
+    });
+
+    document.getElementById('box3').addEventListener('click', function() {
+      window.open('page3.html', '_blank');
+    });
+
+    document.getElementById('box4').addEventListener('click', function() {
+      window.open('page4.html', '_blank');
+    });
+
+    document.getElementById('box5').addEventListener('click', function() {
+      window.open('page5.html', '_blank');
+    });
+
+    document.getElementById('box6').addEventListener('click', function() {
+      window.open('page6.html', '_blank');
+    });
+
+    document.getElementById('box7').addEventListener('click', function() {
+      window.open('page7.html', '_blank');
+    });
+
+    document.getElementById('box8').addEventListener('click', function() {
+      window.open('page8.html', '_blank');
+    });
+
+    document.getElementById('box9').addEventListener('click', function() {
+      window.open('page9.html', '_blank');
+    });
+
+    document.getElementById('box10').addEventListener('click', function() {
+      window.open('page10.html', '_blank');
+    });
+    document.getElementById('box11').addEventListener('click', function() {
+      window.open('page10.html', '_blank');
+    });
+    document.getElementById('box12').addEventListener('click', function() {
+      window.open('page10.html', '_blank');
+    });
+    document.getElementById('box13').addEventListener('click', function() {
+      window.open('page10.html', '_blank');
+    });
+    document.getElementById('box14').addEventListener('click', function() {
+      window.open('page10.html', '_blank');
+    });
+
