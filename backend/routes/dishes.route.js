29c3824365e8c1fc1eaefc79370abf3369be5923
ut-pro <utkarsh.singh144493@gmail.com>
2025-01-19 01:17:08 +0530

    add support for fetching a single dish by ID in dishes API route

--- backend/routes/dishes.route.js
+++ backend/routes/dishes.route.js
@@ -5,11 +5,18 @@ const dishRouter = express.Router();
 
 dishRouter.get("/", async (req, res) => {
   try {
-    const dishes = await Dish.find();
-    res.send(dishes);
+    if (req.query.id){
+      const dish = await Dish.findById(req.query.id);
+      res.send(dish);
+      return;
+    } else {
+      const dishes = await Dish.find();
+      res.send(dishes);
+    }
   } catch (error) {
     console.log(error);
   }
 });
+
 
 export default dishRouter;
